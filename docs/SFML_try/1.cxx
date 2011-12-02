d94f8ab1d626bd9627988993d93bff2f08333b73
F. Thomas <tomtomfatcat@gmail.com>
2011-12-02 20:24:29 +0100

    Essais SFML

--- docs/SFML_try/1.cxx
+++ docs/SFML_try/1.cxx
@@ -1,56 +1,138 @@
-#include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
 #include <iostream>
+#include <sstream>
 using namespace std;
+
+
+using namespace sf;
+
+
 
 
 int main()
 {
   // Create the main window
-  sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+  RenderWindow window(VideoMode(800, 600), "SFML window");
   // Load a sprite to display
-  sf::Texture texture;
+  Texture texture;
   if (!texture.LoadFromFile("protoman.png"))
     return EXIT_FAILURE;
-  sf::Sprite sprite(texture);
+  Sprite sprite(texture);
+  sprite.SetPosition(400,300);
+  
   // Create a graphical text to display
-  sf::Font font;
-  if (!font.LoadFromFile("FreeMono.ttf"))
+
+  int sco = 0;
+
+  
+  Font font;
+  if (!font.LoadFromFile("ARCADE_I.TTF"))
     return EXIT_FAILURE;
-  //  sf::Text text("Hello SFML", font, 50);
-  // Load a music to play
-  // sf::Music music;
+  Text text("0000", font, 25);
+    /// Load a music to play
+  // Music music;
   // if (!music.OpenFromFile("toto.ogg"))
   //   return EXIT_FAILURE;
    // Play the music
   //  music.Play();
   // Start the game loop
-
+  bool sens=true;
+  
   
   while (window.IsOpened())
     {
+
+
+      //movement clavier fluide
+      int x = 0;
+      int y = 0;
+
+      int spx = sprite.GetPosition().x;
+      int spy = sprite.GetPosition().y;
+      
+      if ((spy>=0) &&(spy<=500) )
+	{
+	  
+	  if (Keyboard::IsKeyPressed(Keyboard::Down)) y=15;
+	  if (Keyboard::IsKeyPressed(Keyboard::Up)) y=-15;
+
+	}
+
+      
+      if ((spy+y<0)||(spy+y>500)) y=0;
+
+      
+      //move auto (H)
+      if (sens) {
+	sprite.FlipX(true);
+	
+	if (sprite.GetPosition().x<700) x=3;
+	else sens=!sens;
+
+      }
+      else 
+	{
+	  sprite.FlipX(false);
+	  
+	  if (sprite.GetPosition().x>0) x=-5;
+	  else sens=!sens;
+	}
+      
+
+
+      sprite.Move(x,y);
+
+
       // Process events
-      sf::Event event;
+      Event event;
       while (window.PollEvent(event))
 	{
 	  // Window closed
-	  if (event.Type == sf::Event::Closed)
+	  if (event.Type == Event::Closed)
 	    window.Close();
-	  // q key pressed
-	  if ((event.Type == sf::Event::KeyPressed) && (event.Key.Code == sf::Keyboard::Q))
-	    window.Close();
+
+	  if (event.Type == Event::KeyPressed)
+	    {
+	      switch (event.Key.Code)
+	  	{
+	  	case Keyboard::Q : window.Close();
+	  	  break;
+			  
+	  	}
+	    }
+	  
 	  
 	  // recupere coordonnees souri et bouge le sprite
-	  if (event.Type == sf::Event::MouseButtonPressed)
+	  if (event.Type == Event::MouseButtonPressed)
 	    {
-	      sf::Vector2i position = sf::Mouse::GetPosition(window);
+	      sco++;
+	      //pour avoir un beau score sur 4 digits ^^
+	      ostringstream oss;
+	      if (sco < 10)
+		oss << 0 << 0 << 0 <<sco ;
+	      else if (sco<100)
+		oss << 0 << 0 << sco;
+	      else if (sco<1000)
+		oss << 0 << sco;
+	      else oss << sco;
+	      
+	      text.SetString( oss.str());
+	      cout<<sco<<endl;
+	      
+	      
+	      Vector2i position = Mouse::GetPosition(window);
 	      int x = position.x;
 	      int y = position.y;
-	      cout<<x<<","<<y<<endl;
+
+	      
 	      sprite.SetPosition(x,y);
+	      
+	      
+
 	    }
 	}
-
+      
       // une pause c'est mieux
       usleep(10000);
       // Clear screen
@@ -58,7 +140,7 @@ int main()
       // Draw the sprite
       window.Draw(sprite);
       // Draw the string
-      //  window.Draw(text);
+      window.Draw(text);
       // Update the window
       window.Display();
     }
