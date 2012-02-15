dedd8834e6fb21efbb8422284b31348d5c8b0777
kompote <tomtomfatcat@gmail.com>
2012-02-16 00:43:21 +0100

    [DEL] choix Mob

--- src/PopupMenu.cxx
+++ src/PopupMenu.cxx
@@ -32,11 +32,11 @@ PopupMenu::PopupMenu(int x, int y) {
 	button[1].SetPosition(x+40-button[1].GetGlobalBounds().Width/2,y+30);
 	button[1].SetColor(Color::Black);
 
-	button[2].SetString("Mob");
-	button[2].SetFont(font);
-	button[2].SetCharacterSize(10);
-	button[2].SetPosition(x+40-button[2].GetGlobalBounds().Width/2,y+50);
-	button[2].SetColor(Color::Black);
+	// button[2].SetString("Mob");
+	// button[2].SetFont(font);
+	// button[2].SetCharacterSize(10);
+	// button[2].SetPosition(x+40-button[2].GetGlobalBounds().Width/2,y+50);
+	// button[2].SetColor(Color::Black);
 }
 
 void PopupMenu::Render(RenderTarget& target) const {
